16400bf15b33cbe5edc8c463608fa304476788fa
Ango <pansheng@2008.sina.com>
2023-07-06 11:58:08 +0800

    feat: enqueueUpdate时把update通过next链接形成环状链表

--- packages/react-dom/node_modules/react-reconciler/src/updateQueue.ts
+++ packages/react-dom/node_modules/react-reconciler/src/updateQueue.ts
@@ -3,6 +3,7 @@ import {Action} from 'shared/ReactTypes'
 
 export interface Update<State> {
 	action: Action<State>
+	next: Update<any> | null
 }
 
 export interface UpdateQueue<State> {
@@ -15,7 +16,8 @@ export interface UpdateQueue<State> {
 //  创建Update
 export const createUpdate = <State>(action: Action<State>): Update<State> => {
 	return {
-		action
+		action,
+		next: null
 	}
 }
 
@@ -29,11 +31,27 @@ export const createUpdateQueue = <State>() => {
 	} as UpdateQueue<State>
 }
 
-//  往 updateQueue 里增加 update
+/**
+ * 往 updateQueue 里增加 update
+ * 每次新增Update 不会覆盖原来的
+ * 而是形成一条通过next链接的环装链表
+ */
 export const enqueueUpdate = <State>(
 	updateQueue: UpdateQueue<State>,
 	update: Update<State>
 ) => {
+	//	每次添加进来的update 会和上一次插入的Update 通过next 形成一条环装链表 a->b->c->a
+	const pending = updateQueue.shared.pending
+	if (pending === null) {
+		//	插入第一个update 则目前只有一个update 自己和自己形成环装链表 a->a->a
+		update.next = update
+	} else {
+		// pending是最后一个update 它的next就是第一个
+		update.next = pending.next
+		pending.next = update
+	}
+
+	//	pending始终指向链表的最后一个update
 	updateQueue.shared.pending = update
 }
 
